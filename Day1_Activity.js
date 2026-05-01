7ab54749ec7cdc811b99d77b2c8425a50f55a158
Siva <104338894+Siva78456@users.noreply.github.com>
2026-05-01 20:07:37 +0530

    Add files via upload

---
+++ Day1_Activity.js
@@ -0,0 +1,12 @@
+var firstName = "Siva"
+console.log (typeof(firstName))
+
+var companyName = "Novac"
+console.log (typeof(companyName))
+
+var mobileNumber = 86821
+console.log (typeof(mobileNumber))
+
+var isAutomation = true
+var hasPlaywright
+console.log (typeof(hasPlaywright, isAutomation))
