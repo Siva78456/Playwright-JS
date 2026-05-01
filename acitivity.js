d46b0a6f085dbe88d9cfee6a930d203b10f7bfe3
Siva <104338894+Siva78456@users.noreply.github.com>
2026-05-01 20:05:26 +0530

    Add files via upload

---
+++ acitivity.js
@@ -0,0 +1,13 @@
+function isOddorEven ()
+{
+    for (a=1;a<=19;a++)
+    {
+        if (a%2===1)
+        {
+            console.log("Entered number",+a, "is odd" )
+        } else {
+            console.log("Entered number",+a, "is even")
+        }
+    }
+}
+isOddorEven()
